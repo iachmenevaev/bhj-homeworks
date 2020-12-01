34ed3c404937585f5c6f75bb53c90e48d8e7e640
Ekaterina Iachmeneva <iachmenevaev@mail.ru>
2020-12-01 21:47:30 +0300

    task1_countdown

---
+++ js-features/countdown/task.js
@@ -0,0 +1,12 @@
+// задача 1
+setInterval(time,1000);
+let element= document.getElementById('timer');
+elem = parseInt(element.innerHTML);
+function time(){
+ if(elem!=0){
+ elem -=1;
+ element.textContent = elem;
+ }
+  else   {alert('Вы победили в конкурсе!');
+ }
+}
