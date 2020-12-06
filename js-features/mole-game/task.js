69671f681c11d90ddc72127e41b1a25c0bd07b23
Ekaterina Iachmeneva <iachmenevaev@mail.ru>
2020-12-06 17:07:48 +0300

    first commit mole-game

---
+++ js-features/mole-game/task.js
@@ -0,0 +1,32 @@
+
+for (let index = 1; index <= 9; index++) {
+   function getHole(index){
+   let holeClick = document.getElementById(`hole${index}`);
+   return holeClick;
+ }
+ let hole = getHole(index);
+ let dead = document.getElementById("dead");
+ let lost = document.getElementById("lost");
+
+function holecheck(){
+  if ( hole.className.includes ('hole_has-mole') === true) {
+         let counter = parseInt(dead.innerHTML);   
+          counter+=1;
+          dead.textContent = counter;
+   }else{
+          let counterLost = parseInt(lost.innerHTML);   
+          counterLost+=1;
+          lost.textContent = counterLost;
+  }
+    if (dead.textContent === "10") {
+          alert ("Вы победили!");
+          dead = 0;
+          lost = 0;
+    } else if (lost.textContent === "5") {
+           alert ("Вы проиграли");
+           dead = 0;
+           lost = 0;
+    }
+  }
+    hole.onclick = holecheck;     
+}
