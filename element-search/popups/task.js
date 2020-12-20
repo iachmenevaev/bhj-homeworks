78508ba8d0646c53ca7c7891f1303f1f853883ef
Ekaterina Iachmeneva <iachmenevaev@mail.ru>
2020-12-20 16:55:05 +0300

    commit popups

--- element-search/popups/task.js
+++ element-search/popups/task.js
@@ -1,16 +1,15 @@
 let element = document.getElementById('modal_main');
 element.classList.add('modal_active');
 let cross = Array.from(document.getElementsByClassName('modal__close'));
-console.log(cross);
 cross.forEach(item => item.onclick = () => {
         item.closest('.modal_active ').classList.remove('modal_active');
 })
-let elementtwo = document.getElementsByClassName('show-success');
-let arr = Array.from(elementtwo);
+let elementTwo = document.getElementsByClassName('show-success');
+let arr = Array.from(elementTwo);
 let butn = arr[0];
-let elementthree = document.getElementById('modal_success');
-console.log (butn);
+let elementThree = document.getElementById('modal_success');
 butn.onclick = show;
  function show (){
-         elementthree.classList.add('modal_active');
+        element.classList.remove('modal_active');
+        elementThree.classList.add('modal_active');
  }
