6ef8960360c5c1dbd23f081b3616725be2fa2620
Ekaterina Iachmeneva <iachmenevaev@mail.ru>
2021-01-07 13:22:24 +0300

    third commit interests

--- html-forms/interests/task.js
+++ html-forms/interests/task.js
@@ -1,46 +1,28 @@
-const containerElement = document.querySelector('.interests_main');
+const nodeArray = (selector, parent=document) => [].slice.call(parent.querySelectorAll(selector));
+const allThings = nodeArray('input');
 
-containerElement.addEventListener('click', (e) => {
-  let currentElement = e.target;
-  const { parentElement } = e.target;
-  const labelInnerText = parentElement.innerText;
-  if (
-    labelInnerText === 'Еда'
-    || labelInnerText === 'Домашние животные'
-    || labelInnerText === 'Котики'
-  ) {
-    const childElements = [...parentElement.nextElementSibling.querySelectorAll('label input')];
-    if (currentElement.checked) {
-      childElements.forEach((el) => {
-      
-        el.checked = true;
-      });
-    } else {
-      childElements.forEach((el) => {
-      
-        el.checked = false;
-      });
-    }
-  }
-  while (currentElement) {
-    const parent = currentElement
-      .closest(['ul'])
-      .parentNode.querySelector('input');
-    const siblings = getArrayInputs(
-      'input',
-      parent.closest('li').querySelector('ul'),
-    );
-    const checkStatus = siblings.map((check) => check.checked);
-    const every = checkStatus.every(Boolean);
-    const some = checkStatus.some(Boolean);
+addEventListener('change', e => {
+ let check = e.target;
 
-    parent.checked = every;
-    parent.indeterminate = !every && every !== some;
+  if(allThings.indexOf(check) === -1) return;
 
-    currentElement = currentElement !== parent ? parent : 0;
-  }
-});
+  const children = nodeArray('input', check.closest('li'));
+  children.forEach(child => child.checked = check.checked);
 
-function getArrayInputs(selector, parent = document) {
-  return [...parent.querySelectorAll(selector)];
-}
+   while(check){
+
+const parent   = (check.closest(['ul']).parentNode).querySelector('input');
+const siblings = nodeArray('input', parent.closest('li').querySelector(['ul']));
+const checkStatus = siblings.map(check => check.checked);
+const every  = checkStatus.every(Boolean);
+const some = checkStatus.some(Boolean);   
+
+
+parent.checked = every;		
+parent.indeterminate = !every && every !== some;
+check = check != parent ? parent : 0;  
+ }
+})
+
+
+
