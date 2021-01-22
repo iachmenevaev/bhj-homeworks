c24224c6791f6dab603d4a95f2c20e91ba5a0343
Ekaterina Iachmeneva <iachmenevaev@mail.ru>
2021-01-22 16:07:03 +0300

    second  commit preloader

--- async-requests/preloader/task.js
+++ async-requests/preloader/task.js
@@ -8,27 +8,25 @@ xhr.onreadystatechange = function() {
 xhr.open('GET', 'https://netology-slow-rest.herokuapp.com/',true);
 
 xhr.send();
-xhr.onreadystatechange = function getCurrency() {
+xhr.onreadystatechange = getCurrency;
+ function getCurrency() {
     if(xhr.readyState === 4) {
-        let jsonResponse = xhr.response;
-        let currency= JSON.parse(jsonResponse);
-    const valute  = currency.response;
-    const v = valute.Valute;
+    const v = JSON.parse(xhr.response).response.Valute;
     loaderElement.classList.remove('loader_active');
     let template = '';
-    (Object.values(v)).forEach((element) => {
+    Object.values(v).forEach((element) => {
 template += templateCurrency(element);
         });
  container.insertAdjacentHTML('afterbegin', template);
   }
- function templateCurrency({ CharCode, Value}) {
-      return `
-      <div class="item">
-      <div class="item__code">${CharCode}</div>
-      <div class="item__value">${Value}</div>
-      <div class="item__currency">руб.</div>
-    </div>
-  `;
-  }
  }
+}
+function templateCurrency({ CharCode, Value}) {
+  return `
+  <div class="item">
+  <div class="item__code">${CharCode}</div>
+  <div class="item__value">${Value}</div>
+  <div class="item__currency">руб.</div>
+</div>
+`;
 }
